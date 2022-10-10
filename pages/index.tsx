61613ece183992595f6a783046c23c85d7054e07
Radosvet Petrov <93radosvet@gmail.com>
2022-10-10 12:18:01 +0100

    better mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,17 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <section className="flex flex-rol justify-center items-center h-screen">
+    <section className="bg-gray-400 flex flex-rol justify-center items-center h-screen">
       <main className="z-10 opacity-90">
-        <h1 className="text-center text-8xl text-white bg-blue-800 p-10 border-slate-400 border-b-8 rounded-t-xl">
+        <h1 className="text-3xl text-center sm:text-5xl lg:text-8xl text-white bg-blue-800 p-10 border-slate-400 border-b-8 rounded-t-xl">
           Under Construction
         </h1>
-        <p className="text-7xl text-center text-white bg-blue-400 p-10 border-white border-t-8 rounded-b-xl">
+        <p className="text-3xl sm:text-5xl lg:text-7xl text-center text-white bg-blue-400 p-10 border-white border-t-8 rounded-b-xl">
           Coming soon!
         </p>
       </main>
       <div className="">
-        <Image className="blur-sm brightness-50" src="/img/img.jpg" layout="fill" />
+        <Image className="invisible md:visible blur-sm brightness-50" src="/img/img.jpg" layout="fill" />
       </div>
     </section>
   );
